abdc65d57d1720cc71d20899e6c2a0699819a8d6
Nataliia Kotelnitska <83007830+Natali-Kotelnitska@users.noreply.github.com>
2021-09-25 23:13:03 +0300

    updates js code

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-export default 
+// export default 
 const galleryItems = [
   {
     preview:
@@ -64,3 +64,66 @@ const galleryItems = [
     description: 'Lighthouse Coast Sea',
   },
 ];
+
+// 1. Створення і рендер розмітки по масиву даних galleryItems з app.js і наданим шаблоном.
+
+const galleryItemsContainer = document.querySelector('.js-gallery');
+// console.log(galleryItemsContainer);
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+// console.log(galleryMarkup);
+
+galleryItemsContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryMarkup(items) {
+  return galleryItems.map(({preview, original, description}) => {
+    return `
+    <li class="gallery__item">
+  <a
+    class="gallery__link"
+    href="${original}"
+  >
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>
+`})
+    .join('');
+}
+
+// 2. Реалізація делегування на галереї ul.js-gallery і отримання url великого зображення.
+galleryItemsContainer.addEventListener('click', onGalleryItemClick);
+
+
+function onGalleryItemClick(e) {
+  console.log(e.target); 
+  console.log(e.target.dataset.source);
+
+
+  // const isGalleryItem = e.target.classList.contains('gallery__link');
+  // if (!isGalleryItem) {
+  //   return;
+  // }
+  
+}
+
+
+// ШАБЛОН РОЗМІТКИ
+
+//  <li class="gallery__item">
+//   <a
+//     class="gallery__link"
+//     href="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
+//   >
+//     <img
+//       class="gallery__image"
+//       src="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546__340.jpg"
+//       data-source="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
+//       alt="Tulips"
+//     />
+//   </a>
+// </li>
