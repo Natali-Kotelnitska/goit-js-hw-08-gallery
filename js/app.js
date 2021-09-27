1b52a53c5a19af5f32a8ec8f5daf63a83bc5e2da
Nataliia Kotelnitska <nkotelnicka@gmail.com>
2021-09-27 12:46:07 +0300

    Adds hw-08 answers

--- js/app.js
+++ js/app.js
@@ -1,83 +1,64 @@
-// export default 
+// export default
 const galleryItems = [
   {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg',
+    preview: 'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg',
     original:
       'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg',
     description: 'Hokkaido Flower',
   },
   {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
+    preview: 'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
+    original: 'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
     description: 'Container Haulage Freight',
   },
   {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
+    preview: 'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
+    original: 'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
     description: 'Aerial Beach View',
   },
   {
-    preview:
-      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
+    preview: 'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
+    original: 'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
     description: 'Flower Blooms',
   },
   {
-    preview:
-      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
+    preview: 'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
+    original: 'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
     description: 'Alpine Mountains',
   },
   {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
+    preview: 'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
+    original: 'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
     description: 'Mountain Lake Sailing',
   },
   {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
+    preview: 'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
+    original: 'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
     description: 'Alpine Spring Meadows',
   },
   {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
+    preview: 'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
+    original: 'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
     description: 'Nature Landscape',
   },
   {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
+    preview: 'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
+    original: 'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
     description: 'Lighthouse Coast Sea',
   },
 ];
 
 // 1. Створення і рендер розмітки по масиву даних galleryItems з app.js і наданим шаблоном.
 
-const galleryItemsContainer = document.querySelector('.js-gallery');
-// console.log(galleryItemsContainer);
+const galleryItemsRef = document.querySelector('.js-gallery');
 
 const galleryMarkup = createGalleryMarkup(galleryItems);
-// console.log(galleryMarkup);
-
-galleryItemsContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+galleryItemsRef.insertAdjacentHTML('beforeend', galleryMarkup);
 
 function createGalleryMarkup(items) {
-  return galleryItems.map(({preview, original, description}) => {
-    return `
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
     <li class="gallery__item">
   <a
     class="gallery__link"
@@ -91,55 +72,48 @@ function createGalleryMarkup(items) {
     />
   </a>
 </li>
-`})
+`;
+    })
     .join('');
 }
 
+// 2. Реалізація делегування на галереї ul.js-gallery (і отримання url великого зображення).
+const modalWindowRef = document.querySelector('.js-lightbox');
+const modalBtnRef = document.querySelector('[data-action="close-lightbox"]');
+const imageSourceRef = document.querySelector('.lightbox__image');
+const lightboxContentRef = document.querySelector('.lightbox__overlay');
 
-// 2. Реалізація делегування на галереї ul.js-gallery і отримання url великого зображення.
-galleryItemsContainer.addEventListener('click', onGalleryItemClick);
+galleryItemsRef.addEventListener('click', onOpenModalWindow);
+modalBtnRef.addEventListener('click', onCloseModalBtn);
+lightboxContentRef.addEventListener('click', onCloseLightbox);
 
-
-function onGalleryItemClick(e) {
-  console.log(e.target); 
-  const mainPictureSource = e.target.dataset.source;
-  console.log(mainPictureSource);
-
-  // 3. Відкриття модального вікна при натисканні на елементі галереї.
-  
-  
-  // 4. Підміна значення атрибута src елемента img.lightbox\*\* image.
-  
-  // 5. Закриття модального вікна при натисканні на кнопку button[data - action= ""close - lightbox"].
-  
-// 6. Очищення значення атрибута src елемента img.lightbox\*\*image. Це необхідно для того, щоб при
-//    наступному відкритті модального вікна, поки вантажиться зображення, ми не бачили попереднє.
-
-
-  
-  
-  
-
-  // const isGalleryItem = e.target.classList.contains('gallery__link');
-  // if (!isGalleryItem) {
-  //   return;
-  // }
-  
+// 3. Відкриття модального вікна при натисканні на елементі галереї.
+function onOpenModalWindow(e) {
+  e.preventDefault();
+  console.log(e.target.nodeName);
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  } else {
+    modalWindowRef.classList.add('is-open');
+    //   4. Підміна значення атрибута src елемента img.lightbox\*\* image.
+    imageSourceRef.src = e.target.dataset.source;
+    imageSourceRef.alt = e.target.alt;
+  }
 }
 
+//   5. Закриття модального вікна при натисканні на кнопку
+// button[data - action= ""close - lightbox"].
+function onCloseModalBtn(e) {
+  modalWindowRef.classList.remove('is-open');
+  // 6. Очищення значення атрибута src елемента img.lightbox\*\*image. Це необхідно для того, щоб при
+  //    наступному відкритті модального вікна, поки вантажиться зображення, ми не бачили попереднє.
+  imageSourceRef.src = '';
+  imageSourceRef.alt = '';
+}
 
-// ШАБЛОН РОЗМІТКИ
-
-//  <li class="gallery__item">
-//   <a
-//     class="gallery__link"
-//     href="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
-//   >
-//     <img
-//       class="gallery__image"
-//       src="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546__340.jpg"
-//       data-source="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
-//       alt="Tulips"
-//     />
-//   </a>
-// </li>
+function onCloseLightbox(event) {
+  if (event.currentTarget === event.target) {
+    // console.log('click') - перевірка;
+    onCloseModalBtn();
+  }
+}
